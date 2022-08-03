506f1d8205713d9a755dad41bb86bad7c8777ba8
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-08-03 17:38:50 -0300

    Initial Commit

---
+++ G1NewsRSS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  G1NewsRSS
+//
+//  Created by Felipe Guimarães on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
